75562d873fbd71228c3444b99a56f1184330ef41
Nicolina Wegert <nicolinawegert@gmail.com>
2026-03-21 20:55:23 +0100

    Add IMovieService for movie queries

---
+++ RetroVHS.Api/Services/Movies/IMovieService.cs
@@ -0,0 +1,12 @@
+using RetroVHS.Shared.DTOs.Movies;
+
+namespace RetroVHS.Api.Services.Movies;
+
+/// <summary>
+/// Interface för MovieService som hanterar affärslogik relaterad till filmer.
+/// </summary>
+
+public interface IMovieService
+{
+  Task<List<MovieListDto>> GetAllMoviesAsync(MovieFilterDto filter);
+}
