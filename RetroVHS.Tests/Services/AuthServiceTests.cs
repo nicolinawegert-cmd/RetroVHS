58bd2d40ef0fd2f0ad7092ff95de416923914d45
AlexAC <bigbossalex11@gmail.com>
2026-03-23 13:48:03 +0100

    Add AuthServiceTests constructor setup

--- RetroVHS.Tests/Services/AuthServiceTests.cs
+++ RetroVHS.Tests/Services/AuthServiceTests.cs
@@ -5,8 +5,8 @@ using RetroVHS.Api.Models;
 using RetroVHS.Api.Services.Auth;
 using RetroVHS.Shared.DTOs.Auth;
 using Xunit;
-
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace RetroVHS.Tests.Services
 {
@@ -17,5 +17,31 @@ namespace RetroVHS.Tests.Services
         private readonly Mock<SignInManager<ApplicationUser>> _mockSigninManager;
         private readonly Mock<UserManager<ApplicationUser>> _mockUserManager;
         private readonly ApplicationDbContext _context;
+        private readonly AuthService _authService;
+        public AuthServiceTests()
+        {
+            var store = new Mock<IUserStore<ApplicationUser>>();
+            _mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                store.Object, null, null, null, null, null, null, null, null);
+
+            _mockJwtTokenSerice = new Mock<IJwtTokenService>();
+            _mockConfigartion = new Mock<IConfiguration>();
+            _mockSigninManager = new Mock<SignInManager<ApplicationUser>>(
+            _mockUserManager.Object, null, null, null, null, null, null);
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            _context = new ApplicationDbContext(options);
+
+            _authService = new AuthService(
+                _mockUserManager.Object,
+                _mockSigninManager.Object,
+                _context,
+                _mockJwtTokenSerice.Object,
+                _mockConfigartion.Object);
+
+
+        }
     }
 }
